4376d7449461f9c00fbffcfe1abee3b633948c5b
Kenny <adelekekehinde06@gmail.com>
2025-02-13 23:05:33 +0100

    feat(style): updates to styles

---
+++ packages/playground-ui/src/components/assistant-ui/user-message.tsx
@@ -0,0 +1,21 @@
+import { MessagePrimitive } from '@assistant-ui/react';
+import { FC } from 'react';
+
+export const UserMessage: FC = () => {
+  return (
+    <MessagePrimitive.Root
+      style={{
+        placeItems: 'end',
+      }}
+      className="grid w-full py-4"
+    >
+      {/* <UserActionBar /> */}
+
+      <div className="bg-primary w-fit text-primary-foreground max-w-[calc(var(--thread-max-width)*0.8)] sm:max-w-[70%] break-words rounded-xl px-3 py-2 text-sm col-start-2 row-start-2">
+        <MessagePrimitive.Content />
+      </div>
+
+      {/* <BranchPicker className="col-span-full col-start-1 row-start-3 -mr-1 justify-end" /> */}
+    </MessagePrimitive.Root>
+  );
+};
