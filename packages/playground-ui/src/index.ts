35550054e4cadecb03f2a7e1fa699ccc56978f7a
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2025-02-20 17:52:52 +0100

    domain dir

--- packages/playground-ui/src/index.ts
+++ packages/playground-ui/src/index.ts
@@ -1,4 +1,4 @@
 import './index.css';
 
 export * from './chat-ui/chat';
-export * from './components/agents/agent/index';
+export * from './domains/agents/agent/index';
--- packages/playground-ui/src/types.ts
+++ packages/playground-ui/src/types.ts
@@ -13,3 +13,52 @@ export interface ChatProps {
   memory?: boolean;
   baseUrl?: string;
 }
+
+export type SpanStatus = {
+  code: number;
+};
+
+export type SpanOther = {
+  droppedAttributesCount: number;
+  droppedEventsCount: number;
+  droppedLinksCount: number;
+};
+
+export type SpanEvent = {
+  attributes: Record<string, string | number | boolean | null>[];
+  name: string;
+  timeUnixNano: string;
+  droppedAttributesCount: number;
+};
+
+export type Span = {
+  id: string;
+  parentSpanId: string | null;
+  traceId: string;
+  name: string;
+  scope: string;
+  kind: number;
+  status: SpanStatus;
+  events: SpanEvent[];
+  links: any[]; // You might want to type this more specifically if you have link structure
+  attributes: Record<string, string | number | boolean | null>;
+  startTime: number;
+  endTime: number;
+  duration: number;
+  other: SpanOther;
+  createdAt: string;
+};
+
+export type SpanNode = Span & {
+  children: SpanNode[];
+  relativePercentage?: number;
+};
+
+export type RefinedTrace = {
+  traceId: string;
+  serviceName: string;
+  duration: number;
+  started: number;
+  status: SpanStatus;
+  trace: Span[];
+};
