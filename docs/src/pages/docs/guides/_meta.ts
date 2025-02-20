18341222dda916942aa2104cf6e7dd13c4022b4f
Ward Peeters <ward@coding-tech.com>
2025-02-20 17:58:26 +0100

    add first part of nextjs docs

--- docs/src/pages/docs/guides/_meta.ts
+++ docs/src/pages/docs/guides/_meta.ts
@@ -2,6 +2,7 @@ const meta = {
   "01-chef-michel": "Agents: Chef Michel",
   "02-stock-agent": "Tools: Stock Agent",
   "03-recruiter": "Workflows: AI Recruiter",
+  "04-mastra-nextjs": "Integrate with Next.js",
 };
 
 export default meta;
