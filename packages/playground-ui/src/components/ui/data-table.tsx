c404d7d299d22834436789c1238960d9a298878c
Ehindero Israel <ehinderoisra@Ehinderos-MacBook-Pro.local>
2025-02-25 21:42:29 +0100

    improve data table loading skeleton

--- packages/playground-ui/src/components/ui/data-table.tsx
+++ packages/playground-ui/src/components/ui/data-table.tsx
@@ -178,13 +178,13 @@ export const DataTable = <TData, TValue>({
               <>
                 {Array.from({ length: 5 }).map((_, index) => (
                   <TableRow key={index} className="border-b-gray-6 border-b-[0.1px] text-[0.8125rem]">
-                    <TableCell className="p-0">
+                    <TableCell className="p-2">
                       <Skeleton className="h-8 w-full" />
                     </TableCell>
-                    <TableCell className="p-0">
+                    <TableCell className="p-2">
                       <Skeleton className="h-8 w-full" />
                     </TableCell>
-                    <TableCell className="p-0">
+                    <TableCell className="p-2">
                       <Skeleton className="h-8 w-full" />
                     </TableCell>
                   </TableRow>
